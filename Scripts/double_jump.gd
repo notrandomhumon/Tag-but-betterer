0991a3a11919830782b45e4f4d02983a251b85c3
calvinrsun <notrandomhumon@gmail.com>
2026-09-09 17:07:59 -0700

    ALKSJdfc;alskdjf;lkasdfasd
    a;sdkjf;alksdjf;lkasdnf

--- Scripts/double_jump.gd
+++ Scripts/double_jump.gd
@@ -1,12 +1,13 @@
 extends Area2D
 
-@onready var red_character: CharacterBody2D = %"red_character"
-@onready var blue_character: CharacterBody2D = %"blue_character"
+@onready var red_character: CharacterBody2D = %"red character"
+@onready var blue_character: CharacterBody2D = %"blue character"
 
 func _on_body_entered(body) -> void:
-	if (body.name == 'blue_character'):
+	if body.name == red_character:
 		queue_free()
-		blue_character.double_jump_collected = true
-	elif (body.name == 'red_character'):
-		queue_free()
-		red_character.double_jump_collected = true
+	
+
+	
+	
+	
